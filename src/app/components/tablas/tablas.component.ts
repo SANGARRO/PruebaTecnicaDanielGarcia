40fd9724fe04c72e160ae7b162bfabfc44b1034c
SANGARRO <heneg1019@gmail.com>
2024-08-30 13:48:22 -0500

    ultimo_cambio

--- src/app/components/tablas/tablas.component.ts
+++ src/app/components/tablas/tablas.component.ts
@@ -23,10 +23,13 @@ import { catchError, of } from 'rxjs';
 export class TablasComponent {
   newPokemonName: string = '';
   newPokemonDescription: string = '';
+  newPokemonType: any;
   pokemons: any[] = [];
   completedPokemons: Set<string> = new Set<string>();
   errorMessage: string = '';
   selectedPokemon: any = null;
+  Pokemonfilter: string = '';
+  filteredPokemons: any[] = [];
 
   constructor(private apiService: ConsultasService) {}
 
@@ -47,24 +50,33 @@ export class TablasComponent {
         this.pokemons = data.results.map((pokemon: any) => ({
           ...pokemon,
           url: pokemon.url,
+          type: this.getPokemonType(pokemon.url) || 'unknown',
         }));
+        this.filteredPokemons = this.pokemons;
       });
   }
 
   addPokemon(): void {
     if (this.newPokemonName.trim() && this.newPokemonDescription.trim()) {
+      const type = this.newPokemonType || 'unknown';
+
       this.pokemons.push({
         name: this.newPokemonName,
         url: `https://pokeapi.co/api/v2/pokemon/${this.newPokemonName.toLowerCase()}`,
         description: this.newPokemonDescription,
+        type: type,
       });
+
       this.newPokemonName = '';
       this.newPokemonDescription = '';
+      this.newPokemonType = '';
+      this.filterPokemons();
     }
   }
 
   deletePokemon(url: string): void {
     this.pokemons = this.pokemons.filter((pokemon) => pokemon.url !== url);
+    this.filterPokemons();
   }
 
   markCompleted(url: string): void {
@@ -86,5 +98,25 @@ export class TablasComponent {
 
   closeDialog(): void {
     this.selectedPokemon = null;
+  }
+
+  filterPokemons(): void {
+    if (this.Pokemonfilter === '') {
+      this.filteredPokemons = this.pokemons;
+    } else if (this.Pokemonfilter === 'unknown') {
+      // Filtra Pokémon con tipo no definido
+      this.filteredPokemons = this.pokemons.filter(
+        (pokemon) => !pokemon.type || pokemon.type === 'unknown'
+      );
+    } else {
+      // Filtra por tipo específico
+      this.filteredPokemons = this.pokemons.filter(
+        (pokemon) => pokemon.type === this.Pokemonfilter
+      );
+    }
+  }
+
+  getPokemonType(url: string): string {
+    return 'unknown';
   }
 }
